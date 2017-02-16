a583f8f0bfe666c6025bf6eb9bc68290cb6a8968
David Wickes <dave.wickes@gmail.com>
2017-02-16 12:00:13 +0000

    implements add

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-## TDD in C
+# TDD in C
 
 This is just a starter / example, very small-scale project of doing basic TDD in
 C just using `assert`.
+
+## Running stuff
+`make` will run the tests; there isn't much else.
--- add_test.c
+++ add_test.c
@@ -3,9 +3,14 @@
 #include <stdio.h>
 
 void testExample() { assert(1); }
+void testAdd() {
+  int result = add(5, 2);
+  assert(result == 7);
+}
 
 int main() {
   testExample();
+  testAdd();
   printf("\t\x1b[32mAdd tests pass\x1b[0m\n");
   return 0;
 }
